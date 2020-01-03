28da2b975f99b3bb74f81f1e64e7c2115f089d44
donizero8 <doni.zero8@gmail.com>
2020-01-03 10:30:41 +0700

    second commit

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField, PasswordField, TextField, \
+    SelectField
+from wtforms.validators import Required, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('Username:', validators=[Required()])
+    password = PasswordField('Password:', validators=[Required()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[Required()])
+    password = PasswordField('Password:', validators=[Required(), \
+        EqualTo('confirm', message='Password must match!')])
+    confirm = PasswordField('Password:', validators=[Required()])
+    submit = SubmitField('Register')
+
+class TaskForm(FlaskForm):
+    content = TextField('Description:', validators=[Required()])
+    status = SelectField('Status', choices=[('1','Todo'), ('2','Doing'), ('3', 'Done')], default='1')
+    submit = SubmitField('Submit')
+
+class DeleteForm(FlaskForm):
+    submit = SubmitField('Submit')
